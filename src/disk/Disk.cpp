d37727d98fc28cbc374a00ad684655632b09b72c
haena <108292614+haenalee0721@users.noreply.github.com>
2024-04-17 16:59:19 +0900

    feat: disk read

--- src/disk/Disk.cpp
+++ src/disk/Disk.cpp
@@ -8,9 +8,27 @@ bool Disk::compaction() {
 
 int Disk::read(uint64_t key) {
 
+    //normal SStale 뒤지기
+    for (auto ss : normalSSTables) {
+                // 맵에서 키 검색
+        auto it = ss->ss.find(key);
+        if (it != ss->ss.end()) {
+            return it->second;  // 키를 찾았으면 값 반환
+        }
     }
 
+    //없으면 delay SStale 뒤지기
+    for (auto ss : delaySSTables) {
+        // 맵에서 키 검색
+        auto it = ss->ss.find(key);
+        if (it != ss->ss.end()) {
+            return it->second;  // 키를 찾았으면 값 반환
+        }
     }
+
+    //없어요
+    return -1;
+}
 
 map<uint64_t, int> Disk::range(uint64_t start, uint64_t end) {
     }
