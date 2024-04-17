f3643dc202e34140bdd7fe94f9bb68f194113307
haena <108292614+haenalee0721@users.noreply.github.com>
2024-04-17 16:59:02 +0900

    add: disk function

--- src/disk/Disk.cpp
+++ src/disk/Disk.cpp
@@ -1,15 +1,22 @@
 #include "Disk.h"
 
 
+bool Disk::compaction() {
+    //Todo: 구현
+    return true;
+}
 
+int Disk::read(uint64_t key) {
 
     }
 
     }
 
+map<uint64_t, int> Disk::range(uint64_t start, uint64_t end) {
     }
 
     }
 
+}
     }
 
