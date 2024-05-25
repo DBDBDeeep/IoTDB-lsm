69969fd9c702f62779cdfc6b63c34d44a0224fa1
Nagyeong3 <skrud0318@naver.com>
2024-05-25 16:42:08 +0900

    feat: main 매개변수 받는 작업

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,8 @@ using namespace std;
 void 기본테스트();
 void 딜레이테스트();
 
-int main(){
+int main(int argc, char* argv[]){
+
 
     DataFactory factory;
     Workload workloadA;
@@ -21,16 +22,21 @@ int main(){
     // size_t size = 4 * 1024; // 예: 4KB
     // factory.writeToFile(size);
     // factory.readFromFile(size);
-
-
+    if (argc != 8) {
+        cerr << "Usage: " << argv[0] << " <initDataCount> <numSegments> <outOfOrderRatio> <readProportion> <insertProportion> <singleReadProportion> <rangeReadProportion>" << endl;
+        return 1;
+    }
 
     /**out of order data 생성 및 파일 쓰기**/
     /**parameter 설정
        o3데이터 없이 생성하고 싶으면 outOfOrderRatio = 0, numSegments = 0**/
-    int initDataCount = 10000; // 데이터셋 크기
-    int numSegments = 4; // out of order 세그먼트 개수
-    double outOfOrderRatio = 0.2; // out of order 비율
+    int initDataCount = atoi(argv[1]); // 데이터셋 크기
+    int numSegments = atoi(argv[2]); // out of ordner 세그먼트 개수
+    double outOfOrderRatio = stod(argv[3]); // out of order 비율
     string initfilePath = "../src/test/dataset/initData.txt";
+
+
+
 
     factory.generateDelayedDataset(initDataCount,outOfOrderRatio,numSegments);
     vector<Record> initDataSet = workloadA.readFile(initfilePath);
@@ -40,10 +46,10 @@ int main(){
      * **/
 
     string filePath = "../src/test/dataset/workloadA.txt";
-    double readProportion = 0.2; // 읽기 작업 비율
-    double insertProportion = 0.8; // 삽입 작업 비율
-    double singleReadProportion = 0.5; // 단일 read 작업에 대한 읽기 작업 비율
-    double rangeReadProportion = 0.5; // range 읽기 작업 비율
+    double readProportion = stod(argv[4]); // 읽기 작업 비율
+    double insertProportion = stod(argv[5]); // 삽입 작업 비율
+    double singleReadProportion = stod(argv[6]); // 단일 read 작업에 대한 읽기 작업 비율
+    double rangeReadProportion = stod(argv[7]); // range 읽기 작업 비율
 
     factory.generateWorkloadDataset(initDataSet, filePath, readProportion, insertProportion, singleReadProportion, rangeReadProportion);
     vector<Record> datasetA = workloadA.readFile(filePath);
@@ -72,10 +78,10 @@ int main(){
 
 //    factory.printDelayData();
 
-    CompactionTest compaction;
-    compaction.runCompaction();
+//    CompactionTest compaction;
+//    compaction.runCompaction();
 
-    
+
 
     DBManager* tree = workloadA.getTree();
     tree->printActiveMemtable(false);
